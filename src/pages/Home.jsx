c8c587cbdc36a9bee62b2fa3133c05779369a685
hannah68 <reihaneh.naderi1989@gmail.com>
2022-04-25 19:37:51 +0100

    update db.json file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
 import './Home.css'
+
 import Header from '../components/Header/Header'
 import AboutSection from '../components/About/AboutSection'
 import ReviewSection from '../components/Reviews/ReviewSection'
