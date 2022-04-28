ff9b5504d839c4ecb84b79797134e5b6a56cdbc3
hannah68 <reihaneh.naderi1989@gmail.com>
2022-04-28 12:27:39 +0100

    add config and util files

---
+++ src/config.js
@@ -0,0 +1,22 @@
+export const APIEndPoints = {
+	home: "http://localhost:3030",
+	trip: "http://localhost:3030/tripData",
+	tour: "http://localhost:3030/tourInfo",
+	question: "http://localhost:3030/questions",
+	review: "http://localhost:3030/reviews",
+};
+
+export const PAGE_LINK = {
+	home: "/",
+	signin: "/signin",
+	signup: "/signup",
+	destionation: "/destionation",
+	about: "/about",
+	contact: "/contact",
+};
+
+export const SOCIAL_LINK = {
+	facebook: "https://en-gb.facebook.com/",
+	pinterest: "https://www.pinterest.co.uk/",
+	instagram: "https://www.instagram.com/",
+};
---
+++ src/utils.js
@@ -0,0 +1,17 @@
+export const iconStyle = {
+	paddingRight: "5px",
+	height: "1rem",
+	width: "1.5rem",
+};
+
+export const iconFooterStyle = {
+	position: "absolute",
+	top: "8px",
+	right: "8px",
+};
+
+export const iconquestion = {
+	width: "1.5em",
+	height: "1.5em",
+	color: "#0F0178",
+};
