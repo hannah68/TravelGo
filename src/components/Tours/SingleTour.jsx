79844257c0937063c69b7837a970d5838ac477a5
hannah68 <reihaneh.naderi1989@gmail.com>
2022-04-28 12:27:55 +0100

    apply formatting on files

--- src/components/Tours/SingleTour.jsx
+++ src/components/Tours/SingleTour.jsx
@@ -1,42 +1,60 @@
-import React, { useState } from 'react'
-import {ImgTour, SingleTourContainer, Footer, TourTitle, TourInfos, LeftInfo, RightInfo, TourDescription, ReadMoreButton} from './SingleTourElements'
-import {FaShuttleVan, FaCalendarAlt, FaUsers} from 'react-icons/fa';
+import { useState } from "react";
+import { FaShuttleVan, FaCalendarAlt, FaUsers } from "react-icons/fa";
 
+import {
+	ImgTour,
+	SingleTourContainer,
+	Footer,
+	TourTitle,
+	TourInfos,
+	LeftInfo,
+	RightInfo,
+	TourDescription,
+	ReadMoreButton,
+} from "./SingleTourElements";
 
-function SingleTour({...tour}) {
-    const [readMore, setReadMore] = useState(false);
-    
-    const readMoreHandler = ()=>{
-        setReadMore(!readMore);
-    }
+const SingleTour = ({ ...tour }) => {
+	const [readMore, setReadMore] = useState(false);
 
-    return (
-        <SingleTourContainer>
-            <ImgTour src={tour.image} alt={tour.place}/>
-            <Footer>
-                <TourTitle>{tour.title}</TourTitle>
-                
-                <TourInfos>
-                    <LeftInfo>
-                        <p><FaUsers/> Small group</p>
-                        <p><FaShuttleVan/> Pickup available</p>
-                        <p><FaCalendarAlt/> Duration: {tour.duration} days</p>
-                    </LeftInfo>
-                    <RightInfo>
-                        <p>from £ {tour.price}<br/> per person</p>
-                    </RightInfo>
-                </TourInfos>
- 
-                <TourDescription>
-                    {readMore ? tour.info : `${tour.info.substring(0,225)}...`}
-                    <ReadMoreButton onClick={()=>readMoreHandler(tour.id)}>
-                        {readMore ? 'Show Less' : 'Read More'}
-                    </ReadMoreButton>
-                </TourDescription>
-                
-            </Footer>
-        </SingleTourContainer>
-    )
-}
+	const readMoreHandler = () => {
+		setReadMore(!readMore);
+	};
 
-export default SingleTour
+	return (
+		<SingleTourContainer>
+			<ImgTour src={tour.image} alt={tour.place} />
+			<Footer>
+				<TourTitle>{tour.title}</TourTitle>
+
+				<TourInfos>
+					<LeftInfo>
+						<p>
+							<FaUsers /> Small group
+						</p>
+						<p>
+							<FaShuttleVan /> Pickup available
+						</p>
+						<p>
+							<FaCalendarAlt /> Duration: {tour.duration} days
+						</p>
+					</LeftInfo>
+					<RightInfo>
+						<p>
+							from £ {tour.price}
+							<br /> per person
+						</p>
+					</RightInfo>
+				</TourInfos>
+
+				<TourDescription>
+					{readMore ? tour.info : `${tour.info.substring(0, 225)}...`}
+					<ReadMoreButton onClick={() => readMoreHandler(tour.id)}>
+						{readMore ? "Show Less" : "Read More"}
+					</ReadMoreButton>
+				</TourDescription>
+			</Footer>
+		</SingleTourContainer>
+	);
+};
+
+export default SingleTour;
